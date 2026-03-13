a826682d2a120146f68f0ca18d03fe047bba0d10
Axl Gómez <axljavigomez@gmail.com>
2026-03-13 15:54:46 -0600

    Creación del método EstadoDispositivo que retornará un valor de tipo string dependiendo del valor del promedio

--- Actividad13/Program.cs
+++ Actividad13/Program.cs
@@ -4,8 +4,19 @@
     public double rendimiento1;
     public double rendimiento2;
     public double rendimiento3;
+
     public double CalcularPromedio()
     {
         return (rendimiento1 + rendimiento2 + rendimiento3) / 3;
+    }
+    public string EstadoDispositivo()
+    {
+        if(((rendimiento1 + rendimiento2 + rendimiento3) / 3)>= 80){
+            return "Óptimo";
+        }
+        else
+        {
+            return "Necesita mantenimiento";
+        }
     }
 }
