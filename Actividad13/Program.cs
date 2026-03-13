1a364c1b201e1d7afa4e34031b61fae580fb29cf
Axl Gómez <axljavigomez@gmail.com>
2026-03-13 16:20:53 -0600

    Creación del método MostrarInfo que hace uso de otros métodos y despliega los datos del dispositivo

--- Actividad13/Program.cs
+++ Actividad13/Program.cs
@@ -11,12 +11,19 @@
     }
     public string EstadoDispositivo()
     {
-        if(((rendimiento1 + rendimiento2 + rendimiento3) / 3)>= 80){
+        if(CalcularPromedio() >= 80){
             return "Óptimo";
         }
         else
         {
             return "Necesita mantenimiento";
         }
+    }
+    public void MostrarInfo()
+    {
+        Console.WriteLine($"Nombre: {nombre}");
+        Console.WriteLine($"Rendimiento 1: {rendimiento1} | Rendimiento 2: {rendimiento2} | Rendimiento 3: {rendimiento3}");
+        Console.WriteLine($"Promedio del rendimiento: {CalcularPromedio()}");
+        Console.WriteLine($"Estado del dispositivo: {EstadoDispositivo()}");
     }
 }
