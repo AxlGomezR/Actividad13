66a6015999d27d2defce8e19ce3797718db1c076
Axl Gómez <axljavigomez@gmail.com>
2026-03-13 15:50:19 -0600

    Creación del método CalcularPromedio que retornará el valor del promedio de los rendimientos

--- Actividad13/Program.cs
+++ Actividad13/Program.cs
@@ -4,4 +4,8 @@
     public double rendimiento1;
     public double rendimiento2;
     public double rendimiento3;
+    public double CalcularPromedio()
+    {
+        return (rendimiento1 + rendimiento2 + rendimiento3) / 3;
+    }
 }
